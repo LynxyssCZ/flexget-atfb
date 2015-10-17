d4430bd468e182fb7c0f6d39fc159cc49aadb9cd
LynxyssCZ <LynxyssCZ@gmail.com>
2015-10-17 22:17:50 +0200

    Stuff

--- index.js
+++ index.js
@@ -2,18 +2,48 @@
 require('dotenv').load();
 var Bluebird = require('bluebird');
 var Yaml = require('yamljs');
-var fs = require('fs');
+var fs = Bluebird.promisifyAll(require('fs'));
 var ATFB = require('./src');
+var Shell = require('shelljs');
 
 var usersString = process.env.USERS;
 var shortNames = process.env.SHORT_NAMES.split(';');
+var lastYaml;
+var refreshInterval;
 
-refresh();
+start();
+
+function start() {
+	refreshInterval = setInterval(refresh, process.env.UPDATE_PERIOD);
+	refresh();
+	//Shell.exec('flexget daemon start -d');
+}
+
+function stop() {
+	clearInterval(refreshInterval);
+	refreshInterval = null;
+	Shell.exec('flexget daemon stop');
+	process.exit();
+}
+process.on('SIGINT', stop);
 
 function refresh() {
 	Bluebird.map(usersString.split(';'), readList)
 	.reduce(normalizeList, Object.create(null))
-	.then(writeList);
+	.then(generateYaml)
+	.then(function(yaml) {
+		if (yaml !== lastYaml) {
+			lastYaml = yaml;
+			return updateConfig(yaml);
+		}
+	})
+	.then(function() {
+		Shell.exec('flexget daemon reload');
+	});
+}
+
+function updateConfig(newYaml) {
+	return fs.writeFileAsync(process.env.SERIES_FILE, newYaml);
 }
 
 function normalizeList(items, list) {
@@ -57,7 +87,7 @@ function readList(user) {
 	return ATFB.Reader.read(user);
 }
 
-function writeList(list) {
+function generateYaml(list) {
 	var series = [];
 
 	Object.keys(list).forEach(function(name) {
@@ -72,20 +102,14 @@ function writeList(list) {
 		}
 	});
 
-	fs.writeFile(
-		process.env.SERIES_FILE,
-		Yaml.stringify({
-			series: {
-				anime: series,
-				settings: {
-					anime: {
-						identified_by: 'sequence'
-					}
+	return Yaml.stringify({
+		series: {
+			anime: series,
+			settings: {
+				anime: {
+					identified_by: 'sequence'
 				}
 			}
-		}, 2),
-		function() {
-
 		}
-	);
+	}, 2);
 }
